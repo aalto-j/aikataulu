4bd44c04c1d4d24ff51a5afd25da0c2c79566e65
Juho Aalto <juho.aalto@iki.fi>
2020-09-05 22:43:07 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,143 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import apiService from './services/services.js'
+//import { ApolloProvider } from "react-apollo";
+//import { ApolloProvider } from '@apollo/react-hooks'
+//import { DepartureView } from './DepartureView'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const Clock = ({ time }) => {
+    function with_zero(t) {
+        return (t < 10 ? '0' : '') + t;
+    }
+
+    return (
+        <div className="time">
+            <div className="cal">
+                <p className="date">{time.getDate()}</p>.
+                <p className="month">{time.getMonth() + 1}</p>.
+                <p className="year">{time.getYear() + 1900}</p>
+            </div><div className="clock">
+                <p className="hour">{with_zero(time.getHours())}.</p>
+                <p className="min">{with_zero(time.getMinutes())}.</p>
+                <p className="sec">{with_zero(time.getSeconds())}</p>
+            </div>
+        </div>
+        )
 }
+
+const Weather = ({ weather }) => {
+    function temp() {
+        return (weather.main.temp.toFixed(0))
+    }
+
+    if (weather === null) {
+        return (null)
+    }
+    else {
+        return (
+            <>
+                <div className="wea">
+                    <div>Weather today</div>
+                    <div>
+                        <p className="temperature">{temp()} C</p>
+                        <img className="icon" src={(`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)} />
+                    </div>
+                    <div>{weather.weather[0].description} </div>
+                    <img className="windicon" src="https://img.icons8.com/color/96/000000/windsock.png" />
+                    <p>{weather.wind.speed} m/s{weather.wind.direction}</p>
+                </div>
+                
+            </>
+            )
+    }
+}
+
+const Event = ({ events }) => {
+    if (events === null) {
+        return (null)
+    } else {
+        return (
+            <>
+                {events[0].id}
+            </>
+            )
+    }
+}
+
+
+const Timetable = ({ timetable }) => {
+    if (timetable === null) {
+        return (null)
+    } else {
+        //console.log(timetable)
+        return (null
+            //<div>timetable.data</div>
+        )
+    }
+}
+
+const Abloc = ({ abloc }) => {
+    if (abloc === null) {
+        return (null)
+    } else {
+        console.log(abloc.RestaurantName)
+        return (
+            <div>juu</div>
+        )
+    }
+}
+
+const App = () => {
+
+    const [abloc, setAbloc] = useState(null)
+    const [weather, setWeather] = useState(null)
+    const [events, setEvents] = useState(null)
+    const [time, setTime] = useState(new Date())
+    const [timetable, setTimetable] = useState(null)
+
+    useEffect(() => {
+        apiService
+            .getWeather()
+            .then(res => {
+                setWeather(res)
+            })
+        apiService
+            .getEvents()
+            .then(res => {
+                setEvents(res)
+            })
+        apiService
+            .getAbloc()
+            .then(res => {
+                setAbloc(res)
+            })
+        apiService
+            .getTimetable()
+            .then(res => {
+                setTimetable(res)
+            })
+        }, [])
+
+    setInterval(() => setTime(new Date()), 1000)
+
+    return (
+        <>
+            <div className="container">
+                <div>
+                    <Clock time={time} />
+                    <Weather weather={weather} />
+                    <Event events={events} />
+                    <Timetable timetable={timetable} />
+                    <Abloc abloc={abloc} />
+                </div>
+                </div>
+            </>
+            )
+}
+
+
 
 export default App;
---
+++ src/services/services.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+
+const api_key = 'f98499062b372a377c81b92ccccc5797'
+const capital = 'Helsinki'
+const weatherUrl = (`https://api.openweathermap.org/data/2.5/weather?q=${ capital }&units=metric&appid=${ api_key }`)
+const eventsUrl = ('https://open-api.myhelsinki.fi/v1/events/')
+const ablocUrl = ('https://foodandco.fi/modules/json/json/Index?costNumber=3087&language=fi')
+
+const client = new ApolloClient({
+    uri: `https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql`
+});
+
+const getTimetable = () => (
+    client
+        .query({
+            query: gql`
+        {
+            stops(name: "t��l�ntori") {
+                name
+                stoptimesWithoutPatterns(numberOfDepartures: 3) {
+                    scheduledArrival
+                    realtimeArrival
+                    arrivalDelay
+                    scheduledDeparture
+                    realtimeDeparture
+                    departureDelay
+                    realtime
+                    realtimeState
+                    serviceDay
+                    headsign
+
+                }
+            }
+        }`
+            }
+    )
+        .then(response => {
+            return response
+        })
+    )
+
+
+const getWeather = () => (
+    axios
+        .get(weatherUrl)
+        .then(response => {
+            return (response.data)
+        })
+)
+
+const getAbloc = () => (
+    axios
+        .get(ablocUrl)
+        .then(response => {
+            console.log(response.data)
+            return (response.data)
+        })
+)
+
+const getEvents = () => (
+    axios
+        .get(eventsUrl)
+        .then(response => {
+            return (response.data)
+        })
+)
+
+
+
+export default { getWeather, getEvents, getTimetable, getAbloc}
